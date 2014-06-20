d3424cfcbb9f9b1ddabab74d77670d2bcdb5322d
paskuale75 <pellicanipasquale@gmail.com>
2014-06-20 12:00:36 +0200

    added it-IT language to widget

---
+++ messages/it/widget.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Message translations.
+ *
+ * NOTE, this file must be saved in UTF-8 encoding.
+ */
+return [
+	'Table' => 'Tabella',
+	'ID' => 'ID',
+	'Name' => 'Nome',
+	'Display _MENU_ records per page' => 'Mostra _MENU_ record per pagina',
+	'Nothing found - sorry' => 'Non ho trovato nulla - mi spiace',
+	'Showing page from _START_ to _END_ from _TOTAL_)' => 'Mostra dal START_ al _END_  su _TOTAL_)',
+	'No records available' => 'Nessun record disponibile',
+	'(filtered from _MAX_ total records)' => '(filtrati su _MAX_ record totali)',
+	'Please wait ...' => 'Attendere prego ...',
+	'Search' => 'Trova',
+	'First' => 'Primo',
+	'Back' => 'Precedente',
+	'Next' => 'Prossimo',
+	'Last' => 'Ultimo',
+	'Loading ...' => 'Caricamento ...',
+	'Clear filter' => 'Annulla filtri',
+	'Request failed:' => 'Richiesta fallita:',
+];
